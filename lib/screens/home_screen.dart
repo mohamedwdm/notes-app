6ab819c1d4fe37a1f2441f5b67519509fd54bbe7
mohamedwdm <wdm46857@gmail.com>
2025-08-13 08:03:13 +0300

    build_notes-app

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  static String id = 'Home Page';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Notes App', style: TextStyle(color: Colors.black))),
+    );
+  }
+}
