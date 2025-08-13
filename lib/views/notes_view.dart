b2f9c2aa516085e0673e04b27607c4b47cbeb5ce
mohamedwdm <wdm46857@gmail.com>
2025-08-13 11:20:13 +0300

    add ShowModalBottomSheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -15,7 +16,17 @@ class NotesView extends StatelessWidget {
           borderRadius: BorderRadius.circular(25),
           // keep it circular
         ),
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            context: context,
+            builder: (context) {
+              return AddNoteBottomSheet();
+            },
+          );
+        },
         child: Icon(Icons.add, color: Colors.black),
       ),
       body: const NotesViewBody(),
---
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class AddNoteBottomSheet extends StatelessWidget {
+  const AddNoteBottomSheet({super.key});
+
+// we extract it as separate widget to be with build method to make the changes happen 
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
