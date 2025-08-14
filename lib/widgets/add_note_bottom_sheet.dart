b7bd556889b09b0571f3b06b556576f3dcaad76a
mohamedwdm <wdm46857@gmail.com>
2025-08-14 08:49:51 +0300

    add custom button

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custm_textfield.dart';
+import 'package:notes_app/widgets/custom_button.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
@@ -9,12 +10,20 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-      
-          CustmTextField(),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+        
+            CustmTextField(hintText: 'title'),
+            SizedBox(height: 15),
+            CustmTextField(hintText: 'content', maxLine: 5),
+            SizedBox(height: 30,),
+            CustomButton(),
+            SizedBox(height: 30,),
+
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/custm_textfield.dart
+++ lib/widgets/custm_textfield.dart
@@ -1,30 +1,32 @@
-
 import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustmTextField extends StatelessWidget {
-  const CustmTextField({
-    super.key,
-  });
+  const CustmTextField({super.key, required this.hintText,  this.maxLine = 1});
+
+  final String hintText;
+  final int maxLine;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      maxLines: maxLine,
       cursorColor: kPrimaryColor,
       decoration: InputDecoration(
-        hintText: 'title',
+        hintText: hintText,
+        
         hintStyle: TextStyle(color: kPrimaryColor),
         border: buildBorder(),
         enabledBorder: buildBorder(),
-        focusedBorder: buildBorder(kPrimaryColor)
+        focusedBorder: buildBorder(kPrimaryColor),
       ),
     );
   }
 
-  OutlineInputBorder buildBorder( [color]) {
+  OutlineInputBorder buildBorder([color]) {
     return OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: BorderSide(color:color ?? Colors.white),
-      );
+      borderRadius: BorderRadius.circular(16),
+      borderSide: BorderSide(color: color ?? Colors.white),
+    );
   }
 }
